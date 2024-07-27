1b1d33b0c4332c1e83a720c2a46681b873ca8e2e
Ariful <arifewucse2016@gmail.com>
2024-07-27 12:24:31 +0600

    done

---
+++ app/src/main/java/bd/org/nidscanner/MainActivity.java
@@ -0,0 +1,83 @@
+package bd.org.nidscanner;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+import bd.org.nidscanner.databinding.ActivityMainBinding;
+import bd.org.nidscanner.parser.DataParser;
+import bd.org.nidscanner.parser.NewNidDataParser;
+import bd.org.nidscanner.parser.OldNidDataParser;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.scanBtn.setOnClickListener(v -> {
+            startScan();
+        });
+    }
+
+
+
+    private void startScan() {
+        ScanOptions options = new ScanOptions();
+        options.setDesiredBarcodeFormats(ScanOptions.PDF_417);
+        options.setBeepEnabled(true);
+        options.setBarcodeImageEnabled(false);
+        options.setOrientationLocked(true);
+        options.setPrompt("Place the barcode inside the rectangle to scan");
+        barcodeLauncher.launch(options);
+    }
+
+    // Register the launcher and result handler
+    private final ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(),
+            result -> {
+                if (result == null) {
+                    Toast.makeText(this, "Scan failed. Please try again.", Toast.LENGTH_LONG).show();
+                    promptRetry();
+                } else {
+                    if (result.getContents() == null) {
+                        Toast.makeText(this, "Scan failed. Please try again.", Toast.LENGTH_LONG).show();
+                        promptRetry();
+                    } else {
+                        String rawData = result.getContents();
+                        Utils.CARD_TYPE cardType = Utils.getCardType(rawData);
+                        if (cardType == Utils.CARD_TYPE.SMART_NID_CARD) {
+                            NewNidDataParser parser = new NewNidDataParser(this, rawData);
+                            saveCardData(parser);
+                        } else if (cardType == Utils.CARD_TYPE.OLD_NID_CARD) {
+                            OldNidDataParser parser = new OldNidDataParser(this, rawData);
+                            saveCardData(parser);
+                        } else {
+                            Toast.makeText(this, "Invalid NID Card", Toast.LENGTH_LONG).show();
+                            promptRetry();
+                        }
+                    }
+                }
+            });
+    private void saveCardData(DataParser parser) {
+        String name = parser.getName();
+        String nidNo = parser.getNidNo();
+        String dateOfBirth = parser.getDateOfBirth();
+        String issueDate = parser.getIssueDate();
+        String rawData = parser.getRawData();
+    }
+
+    private void promptRetry() {
+        Toast.makeText(this, "Would you like to retry scanning?", Toast.LENGTH_LONG).show();
+        binding.scanBtn.setText("Retry Scan");
+        binding.scanBtn.setOnClickListener(v -> startScan());
+    }
+}
---
+++ app/src/main/java/bd/org/nidscanner/parser/DataParser.java
@@ -0,0 +1,40 @@
+package bd.org.nidscanner.parser;
+
+import android.content.Context;
+
+/**
+ * NID Data parser base class
+ */
+public abstract class DataParser {
+    private Context context;
+    protected String name;
+    protected String nidNo;
+    protected String dateOfBirth;
+    protected String issueDate;
+    protected String rawData;
+
+    public DataParser(Context context, String rawData) {
+        this.context = context;
+        this.rawData = rawData;
+    }
+
+    public String getRawData() {
+        return rawData;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNidNo() {
+        return nidNo;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getIssueDate() {
+        return issueDate;
+    }
+}
